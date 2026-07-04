09f1231b9b2a63d266eacd01dc8d3a8d0e12c324
Raj Sirohi <rajsirohi2555@gmail.com>
2026-07-04 10:35:24 +0530

    Added Java Codes

--- Core-Java-Practice/GCR-CodeBase/Arrays/Level-1/MultiplicationTable.java
+++ Core-Java-Practice/GCR-CodeBase/Arrays/Level-1/MultiplicationTable.java
@@ -2,23 +2,24 @@ import java.util.Scanner;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
+        
         System.out.print("Enter a number: ");
-        int number = sc.nextInt();
-
-        int[] table = new int[10];
-
+        int number = scanner.nextInt();
+        
+        int[] multiplicationTable = new int[10];
+        
+        // Store multiplication results from 1 to 10
         for (int i = 1; i <= 10; i++) {
-            table[i - 1] = number * i;
+            multiplicationTable[i - 1] = number * i;
         }
-
-        System.out.println("\nMultiplication Table of " + number + ":");
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(number + " * " + i + " = " + table[i - 1]);
+        
+        // Display the multiplication table
+        System.out.println("\n--- Multiplication Table of " + number + " ---");
+        for (int i = 0; i < multiplicationTable.length; i++) {
+            System.out.println(number + " * " + (i + 1) + " = " + multiplicationTable[i]);
         }
-
-        sc.close();
+        
+        scanner.close();
     }
 }
